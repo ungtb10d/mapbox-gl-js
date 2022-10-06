59e7ae65dbbfb6aa71980f0f35f09b32073aae2d
Stepan Kuzmin <stepan.kuzmin@mapbox.com>
2022-10-06 13:00:38 +0300

    Switch Chrome backend back to `swiftshader` in CI

--- test/integration/testem/testem.js
+++ test/integration/testem/testem.js
@@ -133,10 +133,8 @@ const testemConfig = Object.assign({}, defaultTestemConfig);
 if (process.env.RENDER) Object.assign(testemConfig, renderTestemConfig);
 
 if (process.env.CI) {
-    // Set chrome flags for CircleCI to use llvmpipe driver (see https://github.com/mapbox/mapbox-gl-js/pull/10389).
-    const ciTestemConfig = setChromeFlags([ "--disable-backgrounding-occluded-windows", "--ignore-gpu-blocklist", "--use-gl=desktop" ]);
+    const ciTestemConfig = setChromeFlags([ "--disable-backgrounding-occluded-windows" ]);
     Object.assign(testemConfig, ciTestemConfig);
-
 } else if (process.env.RENDER && process.env.USE_ANGLE && ['metal', 'gl', 'vulkan', 'swiftshader', 'gles'].includes(process.env.USE_ANGLE)) {
     // Allow setting chrome flag `--use-angle` for local development on render/query tests only.
     // Search accepted values for `--use-angle` here: https://source.chromium.org/search?q=%22--use-angle%3D%22
